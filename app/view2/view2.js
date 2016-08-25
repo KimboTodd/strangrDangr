7a1df33d9fcddcf8028a9635b3a08c03b64a7bfe
Kim Todd <kimtoddsemail@gmail.com>
2016-08-25 15:53:43 -0700

    I am doing things

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -3,12 +3,58 @@
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
+    $routeProvider.when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+    });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', [function($scope, $uibModal, $log) {
 
-}]);
+    $scope.animationsEnabled = true;
+
+    $scope.open = function(size) {
+
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            // resolve: {
+            //     items: function() {
+            //         return $scope.items;
+            //     }
+            //}
+        });
+
+        modalInstance.result.then(function(selectedItem) {
+            $scope.selected = selectedItem;
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.toggleAnimation = function() {
+        $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+
+}])
+
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+.controller('ModalInstanceCtrl', function($scope, $uibModalInstance) {
+
+
+
+    $scope.ok = function() {
+        $uibModalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
